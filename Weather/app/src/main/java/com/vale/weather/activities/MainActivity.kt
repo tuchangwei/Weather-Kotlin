96e9d9c6316dd3bd2e87be917d5546419e3c335a
Vale <changweitu@gmail.com>
2017-02-06 15:37:39 +0800

    replace findByID to anko.find.

--- Weather/app/src/main/java/com/vale/weather/activities/MainActivity.kt
+++ Weather/app/src/main/java/com/vale/weather/activities/MainActivity.kt
@@ -6,7 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.vale.weather.R
 import com.vale.weather.adapters.ForecastListAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
     private val items = listOf(
@@ -21,7 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
+
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
     }
