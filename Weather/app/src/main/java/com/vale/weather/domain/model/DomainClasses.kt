a9f7ff5be8c9742c508fa96512a1a36241c9c6ae
Vale <changweitu@gmail.com>
2017-02-08 07:40:30 +0800

    operator overloading

--- Weather/app/src/main/java/com/vale/weather/domain/model/DomainClasses.kt
+++ Weather/app/src/main/java/com/vale/weather/domain/model/DomainClasses.kt
@@ -1,7 +1,12 @@
 package com.vale.weather.domain.model
 data class ForecastList(val city: String,
                         val country: String,
-                        val dailyForecast: List<Forecast>)
+                        val dailyForecast: List<Forecast>) {
+    val size: Int
+    get() = dailyForecast.size
+
+    operator fun get(position: Int) = dailyForecast[position]
+}
 data class Forecast(val date: String,
                     val description: String,
                     val high: Int,
